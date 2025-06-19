62a6c08993e7ecd2dcc53b00f9045e5a204d4ba5
joshstromayer <joshuastromayer@gmail.com>
2025-06-19 17:56:34 +0100

    Finish script.py update

--- leetcode/0058-length-of-last-word.py
+++ leetcode/0058-length-of-last-word.py
@@ -1,14 +1,2 @@
 # Problem 0058: Length Of Last Word
 # Your solution:
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        count = 0
-        str = s.split()
-        for char in str[-1]:
-            if char:
-                count+=1
-        return count
